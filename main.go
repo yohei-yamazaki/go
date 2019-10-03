67c9901f11ea23041241adcee6da5626215ff7fb
yamazaki_yohei <y_yamazaki@timedia.co.jp>
2019-10-03 12:51:07 +0900

    変数を使用

--- main.go
+++ main.go
@@ -2,10 +2,27 @@
 /*
 コメント
 */
+
+// １文字目が大文字だったらグローバル変数、小文字だったらローカル変数
 package main
 
 import "fmt"
 
 func main(){
-  fmt.Println("Hello, World!")
+  msg := "hello world"
+  fmt.Println(msg)
+
+  var (
+    a int
+    s string
+  )
+  a = 1
+  s = "s"
+  fmt.Println(a,s)
+
+  b:=10
+  c:=13.5
+  d:="hoge"
+  var e bool
+  fmt.Printf("b: %d, c:%f, d:%s, e:%t \n", b,c,d,e)
 }
